c05d3d18acb3200dc9bd5864fa0eb0db2de33fa0
Adimin2026 <neymarjnribero@gmail.com>
2026-07-14 09:14:43 -0300

    .

--- js/components.js
+++ js/components.js
@@ -211,7 +211,7 @@
             </div>
         </div>
         <div class="footer-bottom">
-            <p>&copy; 2025 IFRO Campus Ariquemes - Todos os direitos reservados.</p>
+            <p>&copy; 2026 IFRO Campus Ariquemes - Todos os direitos reservados.</p>
         </div>
     </div>
 </footer>`;
